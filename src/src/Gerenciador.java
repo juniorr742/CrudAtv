e00651c91a5718ed743858b290abfcc4e70b5b6a
FIC <fic@info.local>
2026-04-02 18:47:30 -0300

    Adicionei novas funções vulcanicas ao código

--- src/src/Gerenciador.java
+++ src/src/Gerenciador.java
@@ -17,14 +17,30 @@ public class Gerenciador {
     }
 
     public void listar () {
-
+        int i = 0;
+       for (String p : produtos){
+           System.out.printf("Produto de número: %d", i);
+           System.out.println(p);
+           i++;
+       }
     }
 
     public void atualizar () {
-
+        System.out.print("Em qual posição está o item que deseja substituir?: ");
+        int indexSub = sc.nextInt();
+        System.out.print("Digite o nome do produto que quer substituir");
+        String nomeProduto = sc.nextLine();
+        System.out.print("Digite o preço:");
+        double preco = sc.nextDouble();
+        System.out.print("Digite a quantidade: ");
+        int quantidade = sc.nextInt();
+        ArrayList<String> substituicao = new ArrayList<>();
+        produtos.set(indexSub, substituicao.toString());
     }
 
     public void deletar () {
-
+        System.out.print("Digite o número do produto que quer deletar: ");
+        int numProduto = sc.nextInt();
+        produtos.remove(numProduto);
     }
 }
