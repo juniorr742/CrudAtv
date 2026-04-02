e199b527e1add695a2ccfd8ef9b19eb624ec1a34
guibenatte <guibenattedev@gmail.com>
2026-04-01 21:54:20 -0300

    Feat metodos CRUD

--- src/src/Gerenciador.java
+++ src/src/Gerenciador.java
@@ -1,3 +1,27 @@
-public class Gerenciador {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Gerenciador {
+    Scanner sc = new Scanner(System.in);
+
+    public void Produto () {
+        int quantidade = sc.nextInt();
+    }
+    ArrayList<> produtos = new ArrayList<>();
+
+    public void cadastrar () {
+
+    }
+
+    public void listar () {
+
+    }
+
+    public void atualizar () {
+
+    }
+
+    public void deletar () {
+
+    }
 }
